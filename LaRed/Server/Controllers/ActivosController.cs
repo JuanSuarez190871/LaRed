19a580f2c8f2b4ca614b014616ae3793dae1c637
juansuarez16 <juancho_1695@hotmail.com>
2023-09-21 09:48:16 -0500

    Creacion admin

--- LaRed/Server/Controllers/ActivosController.cs
+++ LaRed/Server/Controllers/ActivosController.cs
@@ -21,7 +21,77 @@ namespace LaRed.Server.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Activos>>> GetActivos()
         {
-            return await _context.Activos.ToListAsync();
+            try {
+                return await _context.Activos.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        // GET: api/Activos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activos>> GetActivos(int id)
+        {
+            var activos = await _context.Activos.FindAsync(id);
+            if (activos == null)
+            {
+                return NotFound();
+            }
+            return activos;
+        }
+        // PUT: api/Activos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutActivos(int id, Activos activo)
+        {
+            if (id != activo.ID)
+            {
+                return BadRequest();
+            }
+            _context.Entry(activo).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ActivoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+        // POST: api/Activos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Activos>> PostActivos(Activos activo)
+        {
+            _context.Activos.Add(activo);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetActivos", new { id = activo.ID }, activo);
+        }
+        // DELETE: api/Activos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivo(int id)
+        {
+            var activo = await _context.Activos.FindAsync(id);
+            if (activo == null)
+            {
+                return NotFound();
+            }
+            _context.Activos.Remove(activo);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        private bool ActivoExists(int id)
+        {
+            return _context.Activos.Any(e => e.ID == id);
         }
     }
 }
---
+++ LaRed/Server/Controllers/AdminController.cs
@@ -0,0 +1,101 @@
+﻿using LaRed.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AppContext = LaRed.DataAccess.Data.AppContext;
+namespace LaRed.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : Controller
+    {
+        private readonly AppContext _context;
+        public AdminController(AppContext context)
+        {
+
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // GET: api/Estados
+        [HttpGet("Estados")]
+        public async Task<ActionResult<IEnumerable<Estados>>> GetEstados()
+        {
+            try
+            {
+                return await _context.Estados.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        // GET: api/Estados/5
+        [HttpGet("Estados/{id}")]
+        public async Task<ActionResult<Estados>> GetEstado(int id)
+        {
+            var estado = await _context.Estados.FindAsync(id);
+            if (estado == null)
+            {
+                return NotFound();
+            }
+            return estado;
+        }
+        // PUT: api/Estados/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("UpEstados/{id}")]
+        public async Task<IActionResult> PutEstados(int id, Estados estado)
+        {
+            if (id != estado.ID)
+            {
+                return BadRequest();
+            }
+            _context.Entry(estado).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EstadoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+        // POST: api/Estados
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost("Estados")]
+
+        public async Task<ActionResult<Activos>> PostActivos(Estados estado)
+        {
+            _context.Estados.Add(estado);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetActivos", new { id = estado.ID }, estado);
+        }
+        // DELETE: api/Estados/5
+        [HttpDelete("{id}")]
+        [ActionName("DelEstado")]
+        public async Task<IActionResult> DeleteEstado(int id)
+        {
+            var estado = await _context.Estados.FindAsync(id);
+            if (estado == null)
+            {
+                return NotFound();
+            }
+            _context.Estados.Remove(estado);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        private bool EstadoExists(int id)
+        {
+            return _context.Estados.Any(e => e.ID == id);
+        }
+    }
+}
